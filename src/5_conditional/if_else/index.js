d5eb81e923a25d22d9123d0ea60bd88deb34c0e2
Argana Phangquestian <arganaphangquestian@gmail.com>
2023-05-10 21:17:01 +0700

    chore: update if else

--- src/5_conditional/if_else/index.js
+++ src/5_conditional/if_else/index.js
@@ -5,3 +5,17 @@ if (role === "ADMIN") {
 } else {
   console.log("Not Allow");
 }
+
+const age = 24; // adult
+
+if (age > 0 && age <= 5) {
+  console.log("baby");
+} else if (age > 6 && age <= 12) {
+  console.log("child");
+} else if (age > 13 && age <= 18) {
+  console.log("tenage");
+} else if (age > 18) {
+  console.log("adult");
+} else {
+  console.log("newborn");
+}
--- src/9_exercise/readme.md
+++ src/9_exercise/readme.md
@@ -2,8 +2,13 @@
 
 ## List of exercise
 
+### 1 - 5
+
 - [ ] FizzBuzz
 - [ ] Login System
+
+### 6 - 8
+
 - [ ] CRUD (Create-Read-Update-Delete) Todo List
 - [ ] Wallet System
   - [ ] Check Amount
